8e7448d8b765563482ab10f8c9c7a57e747abfaa
robertspb <robertsspb@gmail.com>
2016-11-10 00:08:59 +0300

    v 1.0

--- news-parser.js
+++ news-parser.js
@@ -1,4 +1,4 @@
-// Обработка форм с помощью ExpressJS
+// Парсер новостей
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -17,105 +17,77 @@ app.engine('hbs', template);
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
-// Подключаем роутер - ? (взял из примера в интернете, не понимаю нужен ли?)
-app.use(router);
-
-// Показываем папку, которая должна быть подключена к проекту
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Разбираем application/x-www-form-urlencoded
-app.use( bodyParser.urlencoded() );
-
-// Разбираем application/json
-app.use( bodyParser.json() );
+app.use(router); // Подключаем роутер - ? (взял из примера в интернете, не понимаю нужен ли?)
+app.use(express.static(path.join(__dirname, './public'))); // Показываем папку, которая должна быть подключена к проекту
+app.use( bodyParser.urlencoded() ); // Разбираем application/x-www-form-urlencoded
+app.use( bodyParser.json() ); // Разбираем application/json
 
 router.all('/', function (req, res, next) {
     console.log('Someone made a request!');
     next();
 });
 
-router.get('/*', function (req, res) {
-    // Рендеринг шаблона
-    res.render('index', {
-        categoryType: req.query.category,
-        quantity: req.query.quantity
-    });
-});
-
 router.get('/', function (req, res) {
-    // Рендеринг шаблона
-    res.render('index');
+    if (req.query.category && req.query.quantity) {
+        parser(req, res);
+    } else {
+        res.render('index');
+    }
 });
 
 // Обработка POST запроса
-app.post('/', function (req, res) {
+app.post('/', parser);
 
-    const category = req.body.category;
-    const quantity = req.body.quantity;
+function parser(req, res) {
+
+    if (req.body) {
+        var category = req.body.category;
+    } else { category = req.query.category }
+    //const category = req.body.category;
+    if (req.body) {
+        var quantity = req.body.quantity;
+    } else { quantity = req.query.quantity }
+    //const quantity = req.body.quantity;
+
     const url = 'https://ria.ru/' + category;
-    var data = parser(url, quantity);
 
-    console.log(data);
+    let data = [];
 
-    res.redirect('/?category=' + category + '&quantity=' + quantity);
-
-    res.render('index', {
-        categoryType: req.body.category,
-        img: data.img,
-        title: data.title,
-        text: data.text,
-        time: data.time,
-        date: data.date,
-        comments: data.comments,
-        views: data.views,
-        amount: data.amount
-     });
-    
-});
-
-function parser(url, q)
-{
-    var data = {};
-    data.img = [];
-    data.title = [];
-    data.text = [];
-    data.time = [];
-    data.date = [];
-    data.comments = [];
-    data.views = [];
-    data.amount = [];
-
-    request(url, function (error, response, html)
-    {
-        if (error)
-        {
+    request(url, function (error, response, html) {
+        if (error) {
             throw error;
         }
 
-        if (response.statusCode !== 200)
-        {
+        if (response.statusCode !== 200) {
             console.log('Incorrect statusCode: ' + response.statusCode);
         }
 
-        var $ = cheerio.load(html);
+        let $ = cheerio.load(html);
 
-        // Записываем отдельные значения новостей в массивы
-        $('.b-list__item').each(function (i, elem)
-        {
-            if (i === +q) return false;
-            data.img[i] = $(elem).find('img').attr('src');
-            data.title[i] = $(elem).find('a').text().trim();
-            data.text[i] = $(elem).find('.b-list__item-announce').text().trim();
-            data.time[i] = $(elem).find('.b-list__item-time').text().trim();
-            data.date[i] = $(elem).find('.b-list__item-date').text().trim();
-            data.comments[i] = $(elem).find('.m-comments > .b-statistic__number').text().trim();
-            data.views[i] = $(elem).find('.m-views > .b-statistic__number').text().trim();
-            data.amount[i] = i;
+        // Записываем отдельные значения новостей в объекты
+        $('.b-list__item').each(function (i, elem) {
+            if (i === +quantity) return false;
+
+            data[i] = {
+                img: $(elem).find('img').attr('src'),
+                title: $(elem).find('a').text().trim(),
+                text: $(elem).find('.b-list__item-announce').text().trim(),
+                time: $(elem).find('.b-list__item-time').text().trim(),
+                date: $(elem).find('.b-list__item-date').text().trim(),
+                comments: $(elem).find('.m-comments > .b-statistic__number').text().trim(),
+                views: $(elem).find('.m-views > .b-statistic__number').text().trim(),
+                amount: i
+            }
+        });
+        
+        res.render('index', {
+            categoryType: category,
+            data
         });
 
-    });
+        console.log(data);
 
-    return data;
+    });
 }
 
 app.listen(8000, function () {
